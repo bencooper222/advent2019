baf0b8a90c1b0b7247c08cef6ca4ba16e139f2f9
Benjamin Cooper <bencooper222@gmail.com>
2019-12-03 05:46:12 +0000

    finish of day 2 and full of day 3

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -2,7 +2,7 @@ use std::fs::File;
 use std::io::prelude::*;
 
 fn main() {
-    one_p2();
+    two();
 }
 
 fn one_p1() {
@@ -45,9 +45,49 @@ fn one_p2_recurse(input: i64) -> i64 {
 
     println!("{}", needed_fuel);
 
-    return (if needed_fuel < 0 {
+    return if needed_fuel < 0 {
         0
     } else {
         needed_fuel + one_p2_recurse(needed_fuel)
-    });
+    };
+}
+
+fn two() {
+    let mut file = File::open("blobs/two.txt").expect("Unable to open the file");
+    let mut txt = String::new();
+
+    file.read_to_string(&mut txt).expect("Unable to read file");
+
+    let mut command_arr: Vec<u32> = txt
+        .split(",")
+        .map(|el| el.parse::<u32>().unwrap())
+        .collect();
+
+    let mut i = 0;
+    while i < command_arr.len() {
+        println!("{} {}", i, command_arr[i]);
+
+        let mut dest_idx: usize = 0;
+
+        if i + 3 < command_arr.len() {
+            dest_idx = command_arr[i + 3] as usize;
+        }
+        match command_arr[i] {
+            1 => {
+                command_arr[dest_idx] = command_arr[command_arr[i + 1] as usize]
+                    + command_arr[command_arr[i + 2] as usize]
+            }
+            2 => {
+                command_arr[dest_idx] = command_arr[command_arr[i + 1] as usize]
+                    * command_arr[command_arr[i + 2] as usize]
+            }
+            99 => {
+                break;
+            }
+            _ => println!("Invalid opcode: {}", command_arr[i]),
+        }
+        i += 4; // maybe 3?
+    }
+
+    println!("Result: {}", command_arr[0]);
 }
